6be6a7a31b86a35acb601172cb39882e9cee8b9c
MasterAviral <104678125+MasterAviral@users.noreply.github.com>
2023-01-03 21:39:41 +0530

    Add files via upload

---
+++ PRO-102/Move_Files.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+
+from_dir = "C:/Users/avira/Downloads"
+to_dir = "C:/Users/avira/OneDrive/Desktop/WhiteHat Jr"
+
+list_of_files = os.listdir(from_dir)
+print(list_of_files)
+
+
+for file_name in list_of_files:
+
+    name, extension = os.path.splitext(file_name)
+
+    if extension == '':
+        continue
+    if extension in ['.txt', '.pdf', '.doc', '.docx','.jpeg','jfif']:
+
+        path1 = from_dir + '/' + file_name                              
+        path2 = to_dir + '/' + "Document_Files"                     
+        path3 = to_dir + '/' + "Document_Files" + '/' + file_name   
+        if os.path.exists(path2):
+          print("Moving " + file_name + ".....")
+
+         
+          shutil.move(path1, path3)
+
+        else:
+          os.makedirs(path2)
+          print("Moving " + file_name + ".....")
+          shutil.move(path1, path3)
+
+        
